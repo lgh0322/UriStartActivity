0785260e84a4c2909fe746ceaeb239fba523f9a9
李光浩 <liguanghao@viatomtech.com>
2021-09-14 11:13:54 +0800

    Initial commit

---
+++ app/src/main/java/com/vaca/myapplication/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.vaca.myapplication
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        startActivity(Intent(Intent.ACTION_VIEW,Uri.parse("viatom_xphealth://")))
+    }
+}
